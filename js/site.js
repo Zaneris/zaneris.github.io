5f2f3514a76bf756dd105fd1efa75825d167859c
Ted L <dstrike@gmail.com>
2021-04-27 02:33:23 -0400

    Everything else

---
+++ js/site.js
@@ -0,0 +1,15 @@
+﻿function NavTo(url) {
+    window.location = url;
+}
+
+function startCarousel(id) {
+    let el = document.querySelector(id);
+    let carousel = new bootstrap.Carousel(el);
+    carousel.cycle();
+    console.log("Carousel Started");
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
